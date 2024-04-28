7627db84ffde78242f6e7a184304eda5221334a1
Kingakr <155776214+Kingakr@users.noreply.github.com>
2024-04-28 19:40:13 +0200

    summary

---
+++ task-1.js
@@ -0,0 +1,15 @@
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
+  if (customerCredits >= totalPrice) {
+    console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!`);
+  } else {
+    console.log(`Insufficient funds!`);
+  }
+}
+
+const totalPrice = quantity * pricePerDroid;
+
+makeTransaction(5, 3000, 23000);
+makeTransaction(3, 1000, 15000);
+makeTransaction(10, 5000, 8000);
+makeTransaction(8, 2000, 10000);
+makeTransaction(10, 500, 5000);
